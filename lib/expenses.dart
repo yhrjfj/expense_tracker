b7ccaefbf93aad64fedc77c4b902c766a086297e
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-06 20:09:13 +0600

    Minor change

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -10,7 +10,7 @@ class Expenses extends StatefulWidget {
 class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Column(
         children: [
           Text('The Chart'),
