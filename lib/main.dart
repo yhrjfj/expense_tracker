a0f4092b85012de1157fe23d99836c032b925d9d
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-07 18:14:30 +0600

    Use material 3 style design

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,10 @@ import 'package:expense_tracker/widgets/expenses.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
+      theme: ThemeData(useMaterial3: true),
       debugShowCheckedModeBanner: false,
-      home: Expenses(),
+      home: const Expenses(),
     ),
   );
 }
