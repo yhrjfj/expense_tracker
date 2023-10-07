e8e8abbcd7a730de87c92141afdaec1118f470f9
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-07 17:01:39 +0600

    Add icons according to the category

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
 enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.food_bank,
+  Category.travel: Icons.travel_explore,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
 
 class Expense {
   Expense({
