520df24c067becc7e11513f983a0957204a257e1
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-10 13:59:50 +0600

    After add new expense keyboard will hide automatically

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -76,6 +76,7 @@ class _NewExpenseState extends State<NewExpense> {
           date: _selectedDate!,
           category: _selectedCategory),
     );
+    Navigator.pop(context);
   }
 
   @override
